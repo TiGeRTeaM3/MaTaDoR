3e073f804eefda0af9e5daa708ff2783c945b2ef
TiGeRTeaM3 <36766246+TiGeRTeaM3@users.noreply.github.com>
2018-03-02 01:42:01 +0330

    Add files via upload

---
+++ api/data/config.lua
@@ -0,0 +1,17 @@
+do local _ = {
+  admins = {},
+  disabled_channels = {},
+  enabled_plugins = {
+    "MRCore"
+  },
+  moderation = {
+    data = "/home/matador/MaTaDoR/cli/data/moderation.json"
+  },
+  sudo_users = {
+    205549111,
+    205549111,
+    97476012
+  }
+}
+return _
+end
